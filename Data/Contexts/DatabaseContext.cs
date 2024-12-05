4c72b9df36813509b15a5c63cc573e266755931b
Matheus Akio <matheusakio.dev@gmail.com>
2024-12-05 20:57:58 -0300

    Reapply "adição da migração (ainda não fez update no database, necessário excluir as tabelas antigas)"
    This reverts commit 61934d0a4784cf0ade73a785f88b5528d1676c24.

---
+++ Data/Contexts/DatabaseContext.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppCap7.Models;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<QualidadeArModel> QualidadeAr { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configurando sequência para T_QUALIDADE_AR
+        modelBuilder.HasSequence<int>("SEQ_QUALIDADE_AR")
+            .StartsAt(1)
+            .IncrementsBy(1);
+
+        modelBuilder.Entity<QualidadeArModel>(entity =>
+        {
+            entity.ToTable("T_QUALIDADE_AR");
+
+            entity.Property(e => e.Id)
+                .HasColumnName("ID_DADO")
+                .HasDefaultValueSql("NEXT VALUE FOR SEQ_QUALIDADE_AR")
+                .IsRequired();
+
+            entity.Property(e => e.IdEstacao)
+                .HasColumnName("ID_ESTACAO")
+                .IsRequired();
+
+            entity.Property(e => e.DataHora)
+                .HasColumnName("DATA_HORA")
+                .HasColumnType("TIMESTAMP")
+                .IsRequired();
+
+            entity.Property(e => e.NivelPm25)
+                .HasColumnName("NIVEL_PM2_5")
+                .HasColumnType("FLOAT");
+
+            entity.Property(e => e.NivelPm10)
+                .HasColumnName("NIVEL_PM10")
+                .HasColumnType("FLOAT");
+
+            entity.Property(e => e.ConfigAlertasIdConfiguracao)
+                .HasColumnName("CONFIG_ALERTAS_ID_CONFIGURACAO");
+        });
+
+        // Configurando sequência para TB_USERS
+        modelBuilder.HasSequence<int>("SEQ_USERS")
+            .StartsAt(1)
+            .IncrementsBy(1);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("TB_USERS");
+
+            entity.HasKey(e => e.UserId)
+                .HasName("TB_USERS_PK");
+
+            entity.Property(e => e.UserId)
+                .HasColumnName("USER_ID")
+                .HasDefaultValueSql("NEXT VALUE FOR SEQ_USERS")
+                .IsRequired();
+
+            entity.Property(e => e.Email)
+                .HasColumnName("EMAIL")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            entity.HasIndex(e => e.Email)
+                .IsUnique()
+                .HasDatabaseName("TB_USERS_UNIQUE");
+
+            entity.Property(e => e.PasswordHash) // Alterarei o nome
+                .HasColumnName("PASSWORD")
+                .HasMaxLength(40)
+                .IsRequired();
+
+            entity.Property(e => e.Role)
+                .HasColumnName("ROLE")
+                .HasMaxLength(100)
+                .HasDefaultValueSql("'USER'");
+        });
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,6 +7,7 @@ namespace WebAppCap7.Models
 {
     public class User
     {
+        public int UserId { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string Role { get; set; }
